d47fd50fcd521077e048a09eb8cdb809415447e1
ernesthenry <henry38ernest@gmail.com>
2020-06-29 17:04:38 +0300

    Updates

---
+++ precache-manifest.1b286c01a446b5e1ee51ff9a5c1182bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc360fd7f95e82f3e81b3a76e28ece36",
+    "url": "/chirper/index.html"
+  },
+  {
+    "revision": "97466b8c022402280557",
+    "url": "/chirper/static/css/main.37823b9a.chunk.css"
+  },
+  {
+    "revision": "0b8a88bfd526504ffabd",
+    "url": "/chirper/static/js/2.df6716b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chirper/static/js/2.df6716b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97466b8c022402280557",
+    "url": "/chirper/static/js/main.63cfc3c9.chunk.js"
+  },
+  {
+    "revision": "9347255225c8d4bcd46a",
+    "url": "/chirper/static/js/runtime-main.56b41dcb.js"
+  }
+]);
